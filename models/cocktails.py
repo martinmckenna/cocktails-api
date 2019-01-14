7afec866359af04f46049857262d2bdab734646e
mmckenna <mmckenna.phila@gmail.com>
2019-01-13 23:41:54 -0500

    add additional ingredient info to cocktail

--- models/cocktails.py
+++ models/cocktails.py
@@ -1,8 +1,9 @@
 from flask import Flask
+import json
 from settings import db, ma
 from sqlalchemy import and_
 
-from models.ing_in_cocktails import ing_in_cocktails
+from models.ing_in_cocktails import CocktailIngredient, CocktailIngredientSchema
 from models.ingredients import Ingredient, IngredientSchema
 
 from utils.set_headers import send_200, send_400, send_404
@@ -16,22 +17,22 @@ class Cocktail(db.Model):
     glass = db.Column(db.String(20), nullable=False)
     finish = db.Column(db.String(20), nullable=True)
     ingredients = db.relationship(
-        'Ingredient',
-        secondary=ing_in_cocktails,
-        backref=db.backref('cocktails', lazy='dynamic')
+        'CocktailIngredient',
+        # secondary='ings_in_cocktail',
+        backref=db.backref('cocktails', lazy='joined'),
+        cascade='all, delete-orphan'
+        # primaryjoin=id == CocktailIngredient.cocktail_id
     )
-  
+
     def get_all_cocktails(name=None):
       cocktail_schema = CocktailSchema(strict=True, many=True)
-      # will print kahluha
-      # print(cocktails[0].ingredients[0].name)
 
       # this will return a cocktail as long as any of the ingredients are in it
       # but what we actually want is for a cocktail to be returned ONLY if we have provided
       # all the ingredients
-      thing = Cocktail.query.join(ing_in_cocktails).filter(ing_in_cocktails.columns.ing_id.in_([1, 2]))
-      print(thing)
-      print(thing.all())
+      # thing = Cocktail.query.join(CocktailIngredient).filter(CocktailIngredient.ing_id.in_([3]))
+      # print(thing)
+      # print(cocktail_schema.dump(thing.all()).data)
 
       try:
         # if the client passed a name to search by, use that
@@ -70,6 +71,13 @@ class Cocktail(db.Model):
         if already_exists:
                 return send_400('Invalid Payload', 'Cocktail already exists')
 
+        if not ing_list_is_valid(ing_list):
+          return send_400(
+              "Invalid payload",
+              "Ingredient list invalid. Try the following format: " +
+              "{ 'id': 1, 'ounces': 2.5, 'action': 'muddle', 'step': 1 }"
+          )
+
         ingredients_to_append = generate_ingredients_for_cocktail(ing_list)
 
         if contains_invalid_ingredients(ingredients_to_append) is True:
@@ -102,7 +110,16 @@ class Cocktail(db.Model):
                 return send_400('Invalid Payload', 'Cocktail already exists')
 
         # list of ingredients we want to add
+        # check to see if client has passed ing_list and is not an empty array
         if ing_list is not None and len(ing_list) != 0:
+           # ensure the ingredients is a list of dicts with the valid keys
+            if not ing_list_is_valid(ing_list):
+              return send_400(
+                  "Invalid payload",
+                  "Ingredient list invalid. Try the following format: " +
+                  "{ 'id': 1, 'ounces': 2.5, 'action': 'muddle', 'step': 1 }"
+              )
+
             ingredients_to_append = generate_ingredients_for_cocktail(ing_list)
             if contains_invalid_ingredients(ingredients_to_append) is True:
                 return send_400('Invalid payload', 'Invalid ingredient ID passed', '/cocktails/')
@@ -118,33 +135,65 @@ class Cocktail(db.Model):
         return send_200(cocktail_schema.dump([target_cocktail]).data, '/cocktails/' + str(_id))
 
     def delete_cocktail_by_id(_id):
-        try:
-            cocktail_to_delete = Cocktail.query.filter_by(id=_id).first()
-            if cocktail_to_delete is None:
-                return send_404('/cocktails/' + str(_id))
-            db.session.delete(cocktail_to_delete)
-            db.session.commit()
-            return send_200({}, '/cocktails/' + str(_id))
-        except:
-            return send_400('Something went wrong', 'Could not delete entry', '/cocktails/' + str(_id))
+        cocktail_to_delete = Cocktail.query.filter_by(id=_id).first()
+        if cocktail_to_delete is None:
+            return send_404('/cocktails/' + str(_id))
+        db.session.delete(cocktail_to_delete)
+        db.session.commit()
+        return send_200({}, '/cocktails/' + str(_id))
+        # try:
+        # except:
+            # return send_400('Something went wrong', 'Could not delete entry', '/cocktails/' + str(_id))
 
 
 def generate_ingredients_for_cocktail(ing_list):
-    # client passes an array of ids (ints)
+    # client passes an array of dicts:
+    # [{
+    #   "id": 1,
+    #   "ounces": 2.5,
+    #   "action": 'muddle',
+    #   "step": 1
+    # }]
     # map over that array
-    # for each, get ingredient by ID
+    # for each, validate
     # append the ingredient to the cocktail
     i = 0
     result = []
     while i < len(ing_list):
-        # expecting an array of ingredient ids
-        # @todo be able to pass multiple ids
-        ing_to_commit = Ingredient.get_ingredient_by_id(ing_list[i])
-        result.append(
-            Ingredient.query.filter_by(id=ing_list[i]).first()
+        ing_to_commit = Ingredient.query.filter_by(id=ing_list[i].get('id')).first()
+        (
+            result.append(
+                CocktailIngredient(
+                    ing_id=ing_to_commit.id,
+                    ounces=ing_list[i].get('ounces'),
+                    step=ing_list[i].get('step'),
+                    action=ing_list[i].get('action')
+                )
+            )
+            if ing_to_commit is not None
+            else result.append(None)
         )
         i += 1
     return result
+
+
+def ing_list_is_valid(ing_list):
+  # checks to see if the list of ingredients is made up
+  # of a dicts that have the following keys: id, ounces, action, and step
+  results = []
+  i = 0
+  while i < len(ing_list):
+    (
+        results.append(True)
+        if type(ing_list[i]) is dict
+        and ing_list[i].get('id') is not None
+        and ing_list[i].get('ounces') is not None
+        and ing_list[i].get('action') is not None
+        and ing_list[i].get('step') is not None
+        else results.append(False)
+    )
+    i += 1
+  return all(results)
 
 
 def contains_invalid_ingredients(ing_list):
@@ -155,11 +204,10 @@ def contains_invalid_ingredients(ing_list):
     )
 
 
-
 # Necessary for transforming sqlalchemy data into serialized JSON
 class CocktailSchema(ma.ModelSchema):
     # this is responsible for returning all the ingredient data on the cocktail
-    ingredients = ma.Nested(IngredientSchema, many=True, strict=True)
+    ingredients = ma.Nested(CocktailIngredientSchema, many=True, strict=True)
 
     class Meta:
       model = Cocktail
--- models/ing_in_cocktails.py
+++ models/ing_in_cocktails.py
@@ -1,9 +1,33 @@
 from flask import Flask
 from settings import db, ma
+from marshmallow import fields
 
-ing_in_cocktails = db.Table(
-    'ing_in_cocktails',
-    db.Column('id', db.Integer, primary_key=True),
-    db.Column('cocktail_id', db.Integer, db.ForeignKey('cocktails.id')),
-    db.Column('ing_id', db.Integer, db.ForeignKey('ingredients.id'))
-)
+from models.ingredients import Ingredient, IngredientSchema
+
+class CocktailIngredient(db.Model):
+    __tablename__ = 'ings_in_cocktail'
+    ing_id = db.Column(db.Integer, db.ForeignKey('ingredients.id', ondelete='CASCADE'), primary_key=True)
+    cocktail_id = db.Column(db.Integer, db.ForeignKey('cocktails.id'), primary_key=True)
+    ounces = db.Column(db.Float, nullable=False)
+    action = db.Column(db.String(20), nullable=False)
+    step = db.Column(db.Integer, nullable=False)
+
+    ingredient = db.relationship(
+        'Ingredient',
+        backref=db.backref('ings_in_cocktail', lazy='joined', cascade='delete'),
+    )
+
+# Necessary for transforming sqlalchemy data into serialized JSON
+
+
+class CocktailIngredientSchema(ma.ModelSchema):
+    action = fields.String()
+    ingredient = ma.Nested(IngredientSchema, strict=True)
+    # ingredient = fields.Method("get_ingredients")
+
+    # def get_ingredients(self, obj):
+    #     print(obj.ingredients)
+    #     return {**obj, **obj.ingredient}
+
+    class Meta:
+      model = CocktailIngredient
